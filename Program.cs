288868625cee8fa8dd05646f6bad9c3b659340f3
Jesus <daenatal@yahoo.com>
2022-12-09 07:52:40 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalShoppingListApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApiDbContext>(opt => opt.UseInMemoryDatabase("ShoppingListApi"));
+
+var app = builder.Build();
+
+app.MapGet("/shoppinglist", async (ApiDbContext db) =>
+    await db.Groceries.ToListAsync());
+
+app.MapPost("/shoppinglist", async (Grocery grocery, ApiDbContext db) =>
+    {
+        db.Groceries.Add(grocery);
+
+        await db.SaveChangesAsync();
+
+        return Results.Created($"/shoppinglist/{grocery.Id}", grocery);
+    }
+ );
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
